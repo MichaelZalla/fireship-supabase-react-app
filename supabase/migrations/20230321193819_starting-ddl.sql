c0bae6d2dcff5e02e06eaa93a19355019f6cfd6c
Michael Zalla <michael@zalla.io>
2023-03-22 15:10:50 -0700

    Message board - Normalized data modeling (5);
    Adds `get_single_post_with_comments()`;

--- supabase/migrations/20230321193819_starting-ddl.sql
+++ supabase/migrations/20230321193819_starting-ddl.sql
@@ -122,6 +122,54 @@ begin
 
 end;$$;
 
+-- Returns a specific post and its comment-tree
+
+create function get_single_post_with_comments(post_id uuid)
+returns table (
+	id uuid,
+	author_name text,
+	created_at timestamp with time zone,
+	title text,
+	content text,
+	score int,
+	path ltree
+)
+language plpgsql
+as $$
+begin
+
+	return query
+
+	select
+		posts.id,
+		user_profiles.username,
+		posts.created_at,
+		post_contents.title,
+		post_contents.content,
+		post_score.score,
+		posts.path
+	from posts
+
+	join post_contents on posts.id = post_contents.post_id
+	join post_score on posts.id = post_score.post_id
+	join user_profiles on posts.user_id = user_profiles.user_id
+
+	where
+		posts.path <@ text2ltree(
+			concat(
+				'root.',
+				replace(
+					concat($1, ''),
+					'-',
+					'_'
+				)
+			)
+		)
+	or
+		posts.id = $1;
+
+end;$$;
+
 -- Enables RLS for tables
 
 alter table user_profiles enable row level security;
