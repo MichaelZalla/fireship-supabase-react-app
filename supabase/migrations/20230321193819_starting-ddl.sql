d6ee104e1bea59e79cf1c828944ba7d64c618de5
Michael Zalla <michael@zalla.io>
2023-03-22 15:31:09 -0700

    Message board - Normalized data modeling (7);
    Adds `create_new_comment()`;

--- supabase/migrations/20230321193819_starting-ddl.sql
+++ supabase/migrations/20230321193819_starting-ddl.sql
@@ -189,6 +189,27 @@ begin
 
 end;$$;
 
+-- Create new comment
+
+create function create_new_comment(user_id uuid, content text, path ltree)
+returns boolean
+language plpgsql
+as $$
+begin
+
+	with inserted_post as (
+		insert into posts (user_id, path)
+		values ($1, $3)
+		returning id
+	)
+
+	insert into post_contents (post_id, title, content, user_id)
+		values ((select id from inserted_post), '', $2, $1);
+
+	return true;
+
+end;$$;
+
 -- Enables RLS for tables
 
 alter table user_profiles enable row level security;
